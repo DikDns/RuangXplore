4ad8d6aed9a097011a8942b565827deb6c5e9d3a
RafifZharif <160829784+RafifZharif@users.noreply.github.com>
2025-10-14 01:07:42 +0700

    Update StartScene

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,10 +1,5 @@
 extends Control
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Sekolah.tscn")
 
-func _on_load_pressed() -> void:
-	pass # Replace with function body.
-
-func _on_pengaturan_pressed() -> void:
-	pass # Replace with function body.
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
