4539e55a8316d0fac9273f8d288a8b6c5f8ae01f
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2025-10-25 18:44:56 +0700

    fix: parent and child unsync rotation player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -40,10 +40,12 @@ var _is_jump_pressed = false
 var _is_interact_pressed = false
 var _jump_button_was_pressed = false
 var _interact_button_was_pressed = false
-#------------------------------------------------------------------
+var _sprint_button_was_pressed = false
+
+#--- VAR BARU UNTUK TOGGLE SPRINT ---
+var _sprint_toggled: bool = false
 
 func _ready():
-	# --- KONEKSI KE SINGLETONS ---
 	if BattleManager.has_signal("battle_started"):
 		BattleManager.battle_started.connect(_on_battle_started)
 	BattleManager.battle_ended.connect(_on_battle_ended)
@@ -57,21 +59,15 @@ func _ready():
 		jump_button = mobile_ui.find_child("JumpButton")
 		sprint_button = mobile_ui.find_child("SprintButton")
 		interact_button = mobile_ui.find_child("InteractButton")
-	
-	# Logic mouse capture desktop dihapus
 
 
 func _input(event: InputEvent) -> void:
 	if is_in_battle: return
 	
-	# Logic kamera hanya untuk ScreenDrag (karena mouse emulasi touch)
 	if event is InputEventScreenDrag:
 		view_x.rotation.x -= event.relative.y * camera_sensitivity
 		view_y.rotation.y -= event.relative.x * camera_sensitivity
 		view_x.rotation.x = clampf(view_x.rotation.x, deg_to_rad(-60), deg_to_rad(60))
-	
-	# Logic InputEventMouseMotion dihapus
-
 
 func _process(_delta):
 	# Dari script LAMA
@@ -95,52 +91,46 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, run_speed)
 		velocity.z = move_toward(velocity.z, 0, run_speed)
 
-	move_and_slide() # Dari script BARU
+	move_and_slide() 
 
-
-# Fungsi dari script LAMA
 func _get_inputs():
-	# --- PERBAIKAN 1 ---
-	# Balikin ke standar script "player baru" asli lo: ("ui_up", "ui_down")
-	# "ui_up" (W) akan ngasih nilai -1
 	_input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
-	_is_sprint_pressed = Input.is_action_pressed("ui_sprint") or (sprint_button and sprint_button.is_pressed())
+	var keyboard_sprint_just_pressed = Input.is_action_just_pressed("ui_sprint")
+	var mobile_sprint_just_pressed = (sprint_button and sprint_button.is_pressed() and not _sprint_button_was_pressed)
+
+	if keyboard_sprint_just_pressed or mobile_sprint_just_pressed:
+		_sprint_toggled = not _sprint_toggled
+
+	_is_sprint_pressed = _sprint_toggled
+	
 	_is_jump_pressed = Input.is_action_just_pressed("ui_jump") or (jump_button and jump_button.is_pressed() and not _jump_button_was_pressed)
 	_is_interact_pressed = Input.is_action_just_pressed("ui_accept") or (interact_button and interact_button.is_pressed() and not _interact_button_was_pressed)
 	
 	if jump_button: _jump_button_was_pressed = jump_button.is_pressed()
 	if interact_button: _interact_button_was_pressed = interact_button.is_pressed()
+	if sprint_button: _sprint_button_was_pressed = sprint_button.is_pressed()
 
-
-# Fungsi dari script LAMA (DIMODIFIKASI)
 func handle_movement_input(_delta):
-	# Jump logic
 	if _is_jump_pressed and is_on_floor():
 		velocity.y = jump_speed
 		is_jumping = true
 	
-	# Interact logic
 	if _is_interact_pressed:
 		var actionables = actionable_finder.get_overlapping_areas()
 		if not actionables.is_empty():
 			actionables[0].action()
 
-	# Movement logic
 	if _input_dir.length() > 0:
-		# --- PERBAIKAN 2 ---
-		# Kita pake logic dari script "player baru" asli lo.
-		# Ini udah bener nerjemahin input kamera dan WASD.
 		var direction: Vector3 = (view_y.global_basis * Vector3(_input_dir.x, 0, _input_dir.y)).normalized()
+		var target_rotation = atan2(direction.x, direction.z) + PI
+
+		var desired_local_rotation = target_rotation - self.global_rotation.y
 		
-		# --- MODIFIKASI KUNCI ---
-		# Putar $body, BUKAN si CharacterBody-nya
-		var target_rotation = atan2(direction.x, direction.z)
-		body.rotation.y = lerp_angle(body.rotation.y, target_rotation, 0.1)
+		body.rotation.y = lerp_angle(body.rotation.y, desired_local_rotation, 0.1)
 
 		var current_speed = sprint_speed if _is_sprint_pressed else run_speed
 		
-		# Ganti `movement_dir` jadi `direction`
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
 	else:
@@ -148,29 +138,39 @@ func handle_movement_input(_delta):
 		velocity.z = move_toward(velocity.z, 0, run_speed)
 
 
-# Fungsi dari script LAMA
 func handle_movement_animation():
+	var forward_blend_vec = Vector2(0, -1)
+	var speed_multiplier = _input_dir.length() 
+
 	if is_jumping:
-		if animation_state.get_current_node() != "jump": animation_state.travel("jump")
+		if animation_state.get_current_node() != "jump": 
+			animation_state.travel("jump")
+	
 	elif _input_dir.length() > 0:
 		if _is_sprint_pressed:
-			if animation_state.get_current_node() != "sprint": animation_state.travel("sprint")
+			if animation_state.get_current_node() != "sprint": 
+				animation_state.travel("sprint")
+			
+			animation_tree.set("parameters/sprint/blend_position", forward_blend_vec * speed_multiplier)
+		
 		else:
-			if animation_state.get_current_node() != "walk": animation_state.travel("walk")
+			if animation_state.get_current_node() != "walk": 
+				animation_state.travel("walk")
+			
+			animation_tree.set("parameters/walk/blend_position", forward_blend_vec * speed_multiplier)
+	
 	else:
-		if animation_state.get_current_node() != "idle": animation_state.travel("idle")
-
-#--- FUNGSI BATTLE (DARI SCRIPT LAMA) ---
+		# Player lagi diem
+		if animation_state.get_current_node() != "idle": 
+			animation_state.travel("idle")
 
 func _on_battle_started():
 	is_in_battle = true
 	print("Player entering battle state.")
-	# Input.mouse_mode = Input.MOUSE_MODE_VISIBLE # Dihapus
+	_sprint_toggled = false 
 
 func _on_battle_ended(player_won: bool):
 	is_in_battle = false
-	
-	# Logic mouse capture desktop dihapus
 		
 	if not player_won:
 		current_hp = max_hp
